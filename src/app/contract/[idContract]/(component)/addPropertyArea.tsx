a87b92bbeb0e3a2b37aab5dde244e52ff45ed1b8
Thanh Thanh <thanhthanh186203@gmail.com>
2024-04-24 23:39:30 +0700

    fix button card and textarea of preview contract -- done

--- src/app/contract/[idContract]/(component)/addPropertyArea.tsx
+++ src/app/contract/[idContract]/(component)/addPropertyArea.tsx
@@ -1,75 +1,108 @@
-import ComboboxCustomize from '@/components/ComboBoxCustomize'
-import { Input } from '@/components/ui/input'
-import React, { useState } from 'react'
-import { Button } from '@/components/ui/button';
-
+import ComboboxCustomize from "@/components/ComboBoxCustomize";
+import { Input } from "@/components/ui/input";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 export interface IAddPropertyAreaProps {
-    propertiesCBX: string[]
-    setPropertiesCBX: (value: string[]) => void
-    newPropertiesArray: string[]
-    setNewProperties: (value: string[]) => void,
-    contractAttribute: any,
-    setContractAttribute: (value: any) => void
+  propertiesCBX: string[];
+  setPropertiesCBX: (value: string[]) => void;
+  newPropertiesArray: string[];
+  setNewProperties: (value: string[]) => void;
+  contractAttribute: any;
+  setContractAttribute: (value: any) => void;
 }
 
-export default function AddPropertyArea(
-    { propertiesCBX, setPropertiesCBX, newPropertiesArray, setNewProperties, contractAttribute, setContractAttribute }: IAddPropertyAreaProps
-) {
-    const [properties, setProperty] = useState<string>('')
-    const [inputValue, setInputValue] = useState('')
-    const [propertiesAdd, setPropertiesAdd] = useState<any[]>([
-        {
-            property: '',
-            value: '',
-            isCreated: false
-        }
+export default function AddPropertyArea({
+  propertiesCBX,
+  setPropertiesCBX,
+  newPropertiesArray,
+  setNewProperties,
+  contractAttribute,
+  setContractAttribute,
+}: IAddPropertyAreaProps) {
+  const [properties, setProperty] = useState<string>("");
+  const [inputValue, setInputValue] = useState("");
+  const [propertiesAdd, setPropertiesAdd] = useState<any[]>([
+    {
+      property: "",
+      value: "",
+      isCreated: false,
+    },
+  ]);
+  function handleChange(property: string, value: string) {
+    setInputValue(value);
+    const updatedProperties = propertiesAdd.map((item) => {
+      if (item.property === property) {
+        return { ...item, value: value };
+      }
+      return item;
+    });
+    setPropertiesAdd(updatedProperties);
+  }
+  function handleInputChange(key: any, event: any) {
+    setContractAttribute({ ...contractAttribute, [key]: event });
+  }
+  function removeDuplicates(array: any) {
+    return array.filter(
+      (item: any, index: any) => array.indexOf(item) === index
+    );
+  }
+  function addProperty() {
+    setPropertiesAdd((prevProperties) => [
+      ...prevProperties.slice(0, prevProperties.length - 1),
+      {
+        ...prevProperties[prevProperties.length - 1],
+        isCreated: true,
+        property: properties,
+        value: inputValue,
+      },
+      { property: "", value: "", isCreated: false },
     ]);
-    function handleChange(property: string, value: string) {
-        setInputValue(value);
-        const updatedProperties = propertiesAdd.map((item) => {
-            if (item.property === property) {
-                return { ...item, value: value };
-            }
-            return item;
-        });
-        setPropertiesAdd(updatedProperties);
-    }
-    function handleInputChange(key: any, event: any) {
-        setContractAttribute({ ...contractAttribute, [key]: event });
+    const updatedContractAttribute = {
+      ...contractAttribute,
+      [properties]: inputValue,
     };
-    function removeDuplicates(array: any) {
-        return array.filter((item: any, index: any) => array.indexOf(item) === index);
-    }
-    function addProperty() {
-        setPropertiesAdd(prevProperties => [
-            ...prevProperties.slice(0, prevProperties.length - 1),
-            { ...prevProperties[prevProperties.length - 1], isCreated: true, property: properties, value: inputValue },
-            { property: '', value: '', isCreated: false },
-        ]);
-        const updatedContractAttribute = { ...contractAttribute, [properties]: inputValue };
-        setContractAttribute(updatedContractAttribute);
-        setInputValue('');
-        setProperty('');
-    }
-    return (
-        <div>
-            {propertiesAdd.map((item, index) => (
-                <div className='mt-5' key={index}>
-                    <ComboboxCustomize onSelectedData={setProperty} propertiesCBX={propertiesCBX} setPropertiesCBX={setPropertiesCBX} setNewProperties={setNewProperties} newPropertiesArray={newPropertiesArray}>
-                    </ComboboxCustomize>
-                    <Input onChange={(e) => {
-                        handleChange(item.property, e.target.value)
-                        if (item.isCreated) {
-                            handleInputChange(item.property, e.target.value)
-                        }
-                    }} defaultValue={item.value}>
-                    </Input>
-                </div>
-            ))}
-            <Button variant={'destructive'} onClick={addProperty} type='button'>
-                Add New Property
-            </Button>
+    setContractAttribute(updatedContractAttribute);
+    setInputValue("");
+    setProperty("");
+  }
+  return (
+    <div>
+      {propertiesAdd.map((item, index) => (
+        <div className="mt-3" key={index}>
+          <ComboboxCustomize
+            onSelectedData={setProperty}
+            propertiesCBX={propertiesCBX}
+            setPropertiesCBX={setPropertiesCBX}
+            setNewProperties={setNewProperties}
+            newPropertiesArray={newPropertiesArray}
+          ></ComboboxCustomize>
+          {/* <Input
+            onChange={(e) => {
+              handleChange(item.property, e.target.value);
+              if (item.isCreated) {
+                handleInputChange(item.property, e.target.value);
+              }
+            }}
+            defaultValue={item.value}
+          ></Input> */}
+          <Textarea
+            className="my-3 h-[100px]"
+            placeholder="Nhập các điều khoản chính"
+            onChange={(e) => {
+              handleChange(item.property, e.target.value);
+              if (item.isCreated) {
+                handleInputChange(item.property, e.target.value);
+              }
+            }}
+            defaultValue={item.value}
+          ></Textarea>
         </div>
-    )
+      ))}
+      <Button variant={"destructive"} onClick={addProperty} type="button">
+        Add New Property
+      </Button>
+    </div>
+  );
 }
--- src/app/contract/[idContract]/page.tsx
+++ src/app/contract/[idContract]/page.tsx
@@ -12,90 +12,89 @@ import PreviewContract from "@/app/contract/[idContract]/(component)/PreviewCont
 import { useParams } from "next/navigation";
 
 export default function page() {
-    const [showChat, setShowChat] = useState(false);
-    const { idContract } = useParams();
-    return (
-        <div className="mt-2 overflow-hidden">
-            <div className="grid xl:grid-cols-3 justify-between w-full ">
-                <div className="p-4 w-[500px] col-span-1">
-                    <Card className="rounded-xl border bg-grey-50 text-card-foreground">
-                        <div className="flex flex-col p-6 space-y-1">
-                            <h3 className="font-semibold tracking-tight text-2xl">
-                                Information
-                            </h3>
-                        </div>
-                        <div className="p-6 pt-0 grid gap-4">
-                            <div className="grid gap-2">
-                                <Label htmlFor="name">AddressWallet</Label>
-                                <Input
-                                    className="flex h-10 w-full border border-input rounded-xl"
-                                    id="name"
-                                    placeholder="m@example.com"
-                                    disabled
-                                />
-                            </div>
-                            <div className="grid gap-2">
-                                <Label htmlFor="name">Your Party</Label>
-                                <Input
-                                    className="flex h-10 w-full border border-input rounded-xl"
-                                    id="name"
-                                    placeholder="Your Party"
-                                    disabled
-                                />
-                            </div>
-                            <div className="grid gap-2">
-                                <Label htmlFor="name">Contract Status</Label>
-                                <Input
-                                    className="flex h-10 w-full border border-input rounded-xl"
-                                    id="name"
-                                    placeholder="Contract Status"
-                                    disabled
-                                />
-                            </div>
-                        </div>
+  const [showChat, setShowChat] = useState(false);
+  const { idContract } = useParams();
+  return (
+    <div className="mt-2 overflow-hidden">
+      <div className="grid xl:grid-cols-3 justify-between w-full ">
+        <div className="p-4 w-[500px] col-span-1">
+          <Card className="rounded-xl border bg-grey-50 text-card-foreground">
+            <div className="flex flex-col p-6 space-y-1">
+              <h3 className="font-semibold tracking-tight text-2xl">
+                Information
+              </h3>
+            </div>
+            <div className="p-6 pt-0 grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="name">AddressWallet</Label>
+                <Input
+                  className="flex h-10 w-full border border-input rounded-xl"
+                  id="name"
+                  placeholder="m@example.com"
+                  disabled
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="name">Your Party</Label>
+                <Input
+                  className="flex h-10 w-full border border-input rounded-xl"
+                  id="name"
+                  placeholder="Your Party"
+                  disabled
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="name">Contract Status</Label>
+                <Input
+                  className="flex h-10 w-full border border-input rounded-xl"
+                  id="name"
+                  placeholder="Contract Status"
+                  disabled
+                />
+              </div>
+            </div>
 
-                        <div className="flex items-center px-6 mb-2 justify-between">
-                            <Button className="text-sm bg-green-500 hover:bg-green-500/90 dark:text-white outline-none border-none py-2 px-2 mr-1 border rounded-md shadow flex">
-                                <Check size={17} strokeWidth={2.5} className="mr-1" />
-                                Accept
-                            </Button>
-                            <Button className=" text-sm bg-gradient-to-r  bg-red-500 hover:bg-red-500/90 dark:text-white outline-none border-none py-2 px-2 mr-1 border rounded-md shadow flex">
-                                <X size={17} strokeWidth={2.5} className="mr-1" />
-                                Refuse the invition
-                            </Button>
-                            <Button
-                                className="text-sm  bg-orange-500 hover:bg-orange-500/90 dark:text-white outline-none border-none py-2 px-2 mr-1 border rounded-md shadow flex"
-                                onClick={() => setShowChat(!showChat)}
-                            >
-                                <MessagesSquare size={17} strokeWidth={2.5} className="mr-1" />
-                                Chat with parties
-                            </Button>
-                        </div>
-                        <div className="flex items-center p-3 pt-0 mb-3 mx-10 space-x-2">
-                            <div>
-                                <Link href={`/contract/${idContract}/edit`}>
-                                    <Button className="text-white text-sm bg-blue-500 hover:bg-blue-500/90 dark:text-white outline-none border-none py-2 px-2 mr-3 border rounded-md shadow">
-                                        <FilePen size={17} strokeWidth={2.5} className="mr-1" />
-                                        Edit contract
-                                    </Button>
-                                </Link>
-                            </div>
-                            <div>
-                                <Button className="text-white text-sm bg-blue-500 hover:bg-blue-500/90 dark:text-white outline-none border-none py-2 px-2 mr-3 border rounded-md shadow">
-                                    <FilePen size={17} strokeWidth={2.5} className="mr-1" />
-                                    Sign a contract
-                                </Button>
-                            </div>
-                        </div>
-                        <div className="flex items-center gap-2"></div>
-                    </Card>
-                </div>
-                <div className="p-4 w-[95%] col-span-2 xl:ml-[6%] sm:ml-0">
-                    <PreviewContract></PreviewContract>
-                </div>
-            </div >
-            <ChatBox showChat={showChat} setShowChat={setShowChat}></ChatBox>
-        </div >
-    );
+            <div className="flex items-center px-6 mb-2 justify-between ml-1">
+              <Button className="text-sm bg-green-500 hover:bg-green-500/90 dark:text-white outline-none border-none py-2 px-2 mr-1 border rounded-md shadow flex">
+                <Check size={17} strokeWidth={2.5} className="mr-1" />
+                Accept
+              </Button>
+              <Button className=" text-sm bg-gradient-to-r  bg-red-500 hover:bg-red-500/90 dark:text-white outline-none border-none py-2 px-2 mr-1 border rounded-md shadow flex">
+                <X size={17} strokeWidth={2.5} className="mr-1" />
+                Refuse the invition
+              </Button>
+              <Button
+                className="text-sm  bg-orange-500 hover:bg-orange-500/90 dark:text-white outline-none border-none py-2 px-2 pl-1 mr-1 border rounded-md shadow flex"
+                onClick={() => setShowChat(!showChat)}
+              >
+                <MessagesSquare size={17} strokeWidth={2.5} className="mr-1" />
+                Chat with parties
+              </Button>
+            </div>
+            <div className="flex items-center px-[57px] pt-0 mx-10 mb-6 space-x-2">
+              <div>
+                <Link href={`/contract/${idContract}/edit`}>
+                  <Button className="text-white text-sm bg-gray-500 hover:bg-gray-500/90 outline-none border-none py-2 px-2 border rounded-md shadow">
+                    <FilePen size={17} strokeWidth={2.5} className="mr-1" />
+                    Edit contract
+                  </Button>
+                </Link>
+              </div>
+              <div>
+                <Button className="text-white text-sm bg-blue-500 hover:bg-blue-500/90 dark:text-white outline-none border-none py-2 px-2 border rounded-md shadow">
+                  <FilePen size={17} strokeWidth={2.5} className="mr-1" />
+                  Sign a contract
+                </Button>
+              </div>
+            </div>
+            <div className="flex items-center gap-2"></div>
+          </Card>
+        </div>
+        <div className="p-4 w-[95%] col-span-2 xl:ml-[6%] sm:ml-0">
+          <PreviewContract></PreviewContract>
+        </div>
+      </div>
+      <ChatBox showChat={showChat} setShowChat={setShowChat}></ChatBox>
+    </div>
+  );
 }
-
